9e40aa2f9cb7412c7c9f41fc1fc41ae106ca0620
ZeroneDoo <rizqipambudi6@gmail.com>
2025-02-12 01:10:14 +0700

    add handle for vercel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import komikuRoute from './routes/komiku_route.js'
+import { handle } from 'hono/vercel'
 
 const app = new Hono().basePath('/api')
 
@@ -9,6 +10,8 @@ app.route('/komiku', komikuRoute)
 app.notFound((c) => {
   return c.text('Custom 404 Message', 404)
 })
+
+export default handle(app)
 
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
